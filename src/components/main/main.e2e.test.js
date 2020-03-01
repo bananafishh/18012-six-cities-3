5492e539ef3ec2ed5f7d699a38fe08b9ffb0cd17
z.dyshekova <z.dyshekova@fun-box.ru>
2020-03-02 01:04:37 +0300

    Обновление тестов для компонента «Main».

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -1,36 +1,62 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
+import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Main from './main';
 
-Enzyme.configure({
-  adapter: new Adapter()
-});
+configure({adapter: new Adapter()});
 
-const offersCount = 312;
-
-const offerTitles = [
-  `Beautiful & luxurious apartment at great location`,
-  `Wood and stone place`,
-  `Canal View Prinsengracht`,
-  `Nice, cozy, warm big bed apartment`,
-  `Wood and stone place`
+const offers = [
+  {
+    id: 1,
+    title: `Luxe 1-Bedroom Flat Near Manhattan`,
+    type: `Apartment`,
+    price: 120,
+    picture: `https://placeimg.com/260/200/arch/1`,
+    rating: 4.5,
+    isPremium: true,
+  },
+  {
+    id: 2,
+    title: `Bright & Airy in Highland Park`,
+    type: `Apartment`,
+    price: 200,
+    picture: `https://placeimg.com/260/200/arch/2`,
+    rating: 3.9,
+    isBookmarked: true,
+  },
+  {
+    id: 3,
+    title: `Cozy Parkside Studio in the ❤ of the East Village`,
+    type: `Studio`,
+    price: 500,
+    picture: `https://placeimg.com/260/200/arch/3`,
+    rating: 5,
+    isPremium: true,
+    isBookmarked: true,
+  },
+  {
+    id: 4,
+    title: `Sunny, Modern room in East Village!`,
+    type: `Private room`,
+    price: 80,
+    picture: `https://placeimg.com/260/200/arch/4`,
+    rating: 2.5,
+  },
 ];
 
-it(`При клике по заголовку карточки предложения по аренде вызывается коллбэк`, () => {
-  const offerTitleClickHandler = jest.fn();
+it(`При клике по заголовку карточки предложения об аренде вызывается коллбэк`, () => {
+  const handleOfferTitleClick = jest.fn();
 
   const mainElement = shallow(
       <Main
-        offersCount={offersCount}
-        offerTitles={offerTitles}
-        onOfferTitleClick={offerTitleClickHandler}
+        offers={offers}
+        onOfferTitleClick={handleOfferTitleClick}
       />
   );
 
   const offerTitlesElements = mainElement.find(`.place-card__name`);
   offerTitlesElements.forEach((title) => title.simulate(`click`));
 
-  expect(offerTitleClickHandler).toHaveBeenCalledTimes(offerTitlesElements.length);
+  expect(handleOfferTitleClick).toHaveBeenCalledTimes(offerTitlesElements.length);
 });
