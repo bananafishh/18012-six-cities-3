3698c9aa1fd0d97c9797af1bcb4ae4b18ec87cb6
z.dyshekova <z.dyshekova@fun-box.ru>
2020-03-02 00:53:59 +0300

    Создание моков предложений об аренде.

---
+++ src/mocks/offers.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    title: `Luxe 1-Bedroom Flat Near Manhattan`,
+    type: `Apartment`,
+    price: 120,
+    picture: `https://placeimg.com/260/200/arch/1`,
+    rating: 4.5,
+    isPremium: true,
+  },
+  {
+    id: 2,
+    title: `Bright & Airy in Highland Park`,
+    type: `Apartment`,
+    price: 200,
+    picture: `https://placeimg.com/260/200/arch/2`,
+    rating: 3.9,
+    isBookmarked: true,
+  },
+  {
+    id: 3,
+    title: `Cozy Parkside Studio in the ❤ of the East Village`,
+    type: `Studio`,
+    price: 500,
+    picture: `https://placeimg.com/260/200/arch/3`,
+    rating: 5,
+    isPremium: true,
+    isBookmarked: true,
+  },
+  {
+    id: 4,
+    title: `Sunny, Modern room in East Village!`,
+    type: `Private room`,
+    price: 80,
+    picture: `https://placeimg.com/260/200/arch/4`,
+    rating: 2.5,
+  },
+];
