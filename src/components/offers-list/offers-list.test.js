2e1309cbb20d926a484c46f60aa43ad8a971f377
z.dyshekova <z.dyshekova@fun-box.ru>
2020-03-11 03:17:26 +0300

    Написание тестов для компонента «OffersList».

---
+++ src/components/offers-list/offers-list.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import OffersList from './offers-list.jsx';
+
+const offers = [
+  {
+    id: 1,
+    title: `Luxe 1-Bedroom Flat Near Manhattan`,
+    type: `Apartment`,
+    price: 120,
+    picture: `https://placeimg.com/260/200/arch/1`,
+    rating: 4.5,
+    isPremium: true,
+  },
+  {
+    id: 2,
+    title: `Bright & Airy in Highland Park`,
+    type: `Apartment`,
+    price: 200,
+    picture: `https://placeimg.com/260/200/arch/2`,
+    rating: 3.9,
+    isBookmarked: true,
+  },
+  {
+    id: 3,
+    title: `Cozy Parkside Studio in the ❤ of the East Village`,
+    type: `Studio`,
+    price: 500,
+    picture: `https://placeimg.com/260/200/arch/3`,
+    rating: 5,
+    isPremium: true,
+    isBookmarked: true,
+  },
+  {
+    id: 4,
+    title: `Sunny, Modern room in East Village!`,
+    type: `Private room`,
+    price: 80,
+    picture: `https://placeimg.com/260/200/arch/4`,
+    rating: 2.5,
+  },
+];
+
+it(`Компонент «OffersList» рендерится корректно`, () => {
+  const tree = renderer
+    .create(
+        <OffersList
+          offers={offers}
+          onOfferTitleClick={() => {}}
+        />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
