90150ac9ec8e3a4b56bd3fd79e495992a8f72f8f
z.dyshekova <z.dyshekova@fun-box.ru>
2020-03-16 01:51:50 +0300

    Создание компонента «Review».

--- src/constants.js
+++ src/constants.js
@@ -11,3 +11,23 @@ export const MapPinIcon = {
   URL: `img/pin.svg`,
   SIZE: [27, 39],
 };
+
+export const MONTHS = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+export const DateFormat = {
+  FULL: `MMMM DD, YYYY`,
+  WITH_DASHES: `YYYY-MM-DD`,
+};
