c7a78a086fda3cd22c75c6cb3b0f86be356ffebb
z.dyshekova <z.dyshekova@fun-box.ru>
2020-03-16 01:48:50 +0300

    Добавление отзывов в моки предложений.

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -36,6 +36,31 @@ export default [
       isSuper: true,
     },
     coords: [52.3909553943508, 4.85309666406198],
+    reviews: [
+      {
+        id: 1,
+        text: `Excellent location in a charming building. It’s about the size of a typical hotel room and 
+        perhaps a little smaller than Airbnb places tend to be overall. Would recommend though as it’s 
+        a great value and location.`,
+        rating: 5,
+        date: new Date(`2020-02-15`),
+        user: {
+          name: `Steven`,
+          picture: `https://api.adorable.io/avatars/128/${Math.random()}`,
+        },
+      },
+      {
+        id: 2,
+        text: `Apartment was very pretty and quaint! Location was excellent , very close to the main 
+        sights in Porto. Abott greeted us and was very helpful, gave us some great tips.`,
+        rating: 4,
+        date: new Date(`2020-03-15`),
+        user: {
+          name: `Monica`,
+          picture: `https://api.adorable.io/avatars/128/${Math.random()}`,
+        },
+      },
+    ],
   },
   {
     id: 2,
@@ -71,6 +96,20 @@ export default [
       isSuper: true,
     },
     coords: [52.369553943508, 4.85309666406198],
+    reviews: [
+      {
+        id: 1,
+        text: `Excellent location in a charming building. It’s about the size of a typical hotel room and 
+        perhaps a little smaller than Airbnb places tend to be overall. Would recommend though as it’s 
+        a great value and location.`,
+        rating: 5,
+        date: new Date(`2020-02-15`),
+        user: {
+          name: `Steven`,
+          picture: `https://api.adorable.io/avatars/128/${Math.random()}`,
+        },
+      },
+    ],
   },
   {
     id: 3,
@@ -107,6 +146,19 @@ export default [
       isSuper: false,
     },
     coords: [52.3909553943508, 4.929309666406198],
+    reviews: [
+      {
+        id: 2,
+        text: `Apartment was very pretty and quaint! Location was excellent , very close to the main 
+        sights in Porto. Abott greeted us and was very helpful, gave us some great tips.`,
+        rating: 4,
+        date: new Date(`2020-03-15`),
+        user: {
+          name: `Monica`,
+          picture: `https://api.adorable.io/avatars/128/${Math.random()}`,
+        }
+      },
+    ],
   },
   {
     id: 4,
@@ -141,5 +193,6 @@ export default [
       isSuper: false,
     },
     coords: [52.3809553943508, 4.939309666406198],
+    reviews: [],
   },
 ];
