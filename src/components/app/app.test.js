ae3ca358378a307d4f4a16b2ef65acfd855ca705
z.dyshekova <z.dyshekova@fun-box.ru>
2020-03-19 20:56:46 +0300

    Обновление тестов для компонента «App».

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 import App from './app';
 
-const offers = [
+const mockStore = configureStore([]);
+
+const offers = {
+  Amsterdam: {
+    city: {
+      name: `Amsterdam`,
+      coords: [52.37403, 4.88969],
+    },
+    offers: [
+      {
+        id: 1,
+        title: `Luxe 1-Bedroom Flat Near Manhattan`,
+        type: `apartment`,
+        price: 120,
+        pictures: [`https://placeimg.com/260/200/arch/1`],
+        rating: 4.5,
+        isPremium: true,
+        isBookmarked: false,
+      },
+    ],
+  },
+};
+
+const currentOffers = [
   {
     id: 1,
     title: `Luxe 1-Bedroom Flat Near Manhattan`,
@@ -46,12 +71,23 @@ const offers = [
   },
 ];
 
+const currentCity = {
+  name: `Amsterdam`,
+  coords: [52.37403, 4.88969],
+};
+
 it(`Компонент «App» рендерится корректно`, () => {
+  const store = mockStore({
+    currentCity,
+    currentOffers,
+    offers,
+  });
+
   const tree = renderer
     .create(
-        <App
-          offers={offers}
-        />
+        <Provider store={store}>
+          <App/>
+        </Provider>
     )
     .toJSON();
 
