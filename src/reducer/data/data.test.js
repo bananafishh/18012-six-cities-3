64aae786c1161ff7c2d495abe74fb6b74442b0eb
z.dyshekova <z.dyshekova@fun-box.ru>
2020-04-04 16:45:39 +0300

    Написание тестов на загрузку предложений об аренде.

--- src/reducer/data/data.test.js
+++ src/reducer/data/data.test.js
@@ -1,4 +1,47 @@
-import {reducer, ActionType, ActionCreator} from './data';
+import MockAdapter from "axios-mock-adapter";
+
+import {createApi} from '../../api';
+import {reducer, ActionType, Operation, ActionCreator} from './data';
+import OffersDataAdapter from '../../adapters/offers-data-adapter';
+
+const api = createApi(() => {});
+
+const apiOffers = [
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10,
+      },
+      'name': `Amsterdam`,
+    },
+    'description': `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    'goods': [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+    'host': {
+      'avatar_url': `img/1.png`,
+      'id': 3,
+      'is_pro': true,
+      'name': `Angelina`,
+    },
+    'id': 1,
+    'images': [`img/1.png`, `img/2.png`],
+    'is_favorite': false,
+    'is_premium': false,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8,
+    },
+    'max_adults': 4,
+    'preview_image': `img/1.png`,
+    'price': 120,
+    'rating': 4.8,
+    'title': `Beautiful & luxurious studio at great location`,
+    'type': `apartment`,
+  },
+];
 
 const offers = [
   {
@@ -58,6 +101,29 @@ describe(`Редьюсер «data» работает корректно`, () => 
   });
 });
 
+describe(`Загрузка данных с сервера происходит корректно`, () => {
+  it(`Происходит корректный запрос к API по адресу /hotels`, function () {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const loadOffers = Operation.loadOffers();
+    const adaptedOffers = OffersDataAdapter.parseOffers(apiOffers);
+
+    apiMock
+      .onGet(`/hotels`)
+      .reply(200, apiOffers);
+
+    return loadOffers(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(2);
+
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_OFFERS,
+          payload: adaptedOffers,
+        });
+      });
+  });
+});
+
 describe(`Action creator работает корректно`, () => {
   it(`Action creator для загрузки списка предложений об аренде возвращает правильный action`, () => {
     expect(ActionCreator.loadOffers(offers)).toEqual({
@@ -66,3 +132,5 @@ describe(`Action creator работает корректно`, () => {
     });
   });
 });
+
+
