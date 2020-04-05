827be716a729a1219fbb004d023c91794aa917ef
z.dyshekova <z.dyshekova@fun-box.ru>
2020-04-06 00:16:20 +0300

    Рефакторинг компонента «Main».

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -56,63 +56,32 @@ const Main = (props) => {
   );
 
   return (
-    <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width="81"
-                  height="41"
+    <main className={`page__main page__main--index${!offersLength ? ` page__main--index-empty` : ``}`}>
+      <h1 className="visually-hidden">Cities</h1>
+
+      <CitiesList
+        cities={cities}
+        currentCity={currentCity}
+        onCityChange={onCityChange}
+      />
+
+      <div className="cities">
+        <div className={`cities__places-container${!offersLength ? ` cities__places-container--empty` : ``} container`}>
+          {renderOffers()}
+
+          <div className="cities__right-section">
+            {!!offersLength && (
+              <section className="cities__map map">
+                <Map
+                  offers={offers}
+                  activeOfferId={activeOfferId}
                 />
-              </a>
-            </div>
-
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
+              </section>
+            )}
           </div>
         </div>
-      </header>
-
-      <main className={`page__main page__main--index${!offersLength ? ` page__main--index-empty` : ``}`}>
-        <h1 className="visually-hidden">Cities</h1>
-
-        <CitiesList
-          cities={cities}
-          currentCity={currentCity}
-          onCityChange={onCityChange}
-        />
-
-        <div className="cities">
-          <div className={`cities__places-container${!offersLength ? ` cities__places-container--empty` : ``} container`}>
-            {renderOffers()}
-
-            <div className="cities__right-section">
-              {!!offersLength && (
-                <section className="cities__map map">
-                  <Map
-                    offers={offers}
-                    activeOfferId={activeOfferId}
-                  />
-                </section>
-              )}
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 };
 
